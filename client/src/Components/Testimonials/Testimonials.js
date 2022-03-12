c72bf8624b33dc8ef9d6d0cc8af0e631ba1c7b7e
Ahmed Faheem <ahmedfaheemmalick@gmail.com>
2022-03-12 17:56:28 +0500

    some changes

--- client/src/Components/Testimonials/Testimonials.js
+++ client/src/Components/Testimonials/Testimonials.js
@@ -1,5 +1,6 @@
 import Slider from "react-slick";
 import lady from "../../Images/lady.png"
+import client1 from "../../Images/client1.jpeg"
 import styles from "./Testimonials.module.css"
 
 const Testimonial = () => {
@@ -19,9 +20,9 @@ const Testimonial = () => {
                 <p className={styles.para}>What my clients say about me.</p>
                 <Slider {...settings} className={styles.slider}>
                     <div className={styles.slide}>
-                        <img src={lady} alt="" />
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus dolorum omnis vel, explicabo ab possimus similique consectetur tempora at voluptas.</p>
-                        <span>- Jhon Doel, ABC Company</span>
+                        <img src={client1} alt="" />
+                        <p>A++ Person.. very knowledgeable. Trained me how to use.. very comfortable to work with.. pleased to work with him..</p>
+                        <p>Sathvik - <span>India</span></p>
                     </div>
                     <div className={styles.slide}>
                         <img src={lady} alt="" />
